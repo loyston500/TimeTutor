4128328e1f35fb108d74bc77c4f411ea66632e3d
loyston500 <loyston500@gmail.com>
2023-01-13 23:35:23 +0530

    changes

--- lib/pages/main_settings.dart
+++ lib/pages/main_settings.dart
@@ -1,15 +1,24 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:timetutor/main.dart';
+import 'package:timetutor/themes/themes.dart';
+import 'dart:math' as math;
 
 class MainSettings {
   bool displayPrevPeriod = true;
-  bool autoPlayPeriodsAnimation = false;
+  bool timetableAutoPlayPeriodsAnimation = false;
+  int timetableAutoPlayInterval = 3;
+  bool timetableEnableInfiniteScroll = true;
+  bool darkMode = true;
 }
 
 class MainSettingsPage extends StatefulWidget {
-  const MainSettingsPage({super.key, required this.settings});
+  const MainSettingsPage(
+      {super.key, required this.settings, required this.root});
 
   final MainSettings settings;
+  final AppState Function() root;
 
   @override
   State<MainSettingsPage> createState() => _MainSettingsPageState();
@@ -22,6 +31,11 @@ class _MainSettingsPageState extends State<MainSettingsPage> {
       appBar: AppBar(title: const Text("Settings")),
       body: SettingsList(
         darkTheme: SettingsThemeData(
+          settingsTileTextColor: Theme.of(context).textTheme.bodyText2!.color,
+          titleTextColor: Theme.of(context).iconTheme.color,
+          settingsListBackground: Theme.of(context).backgroundColor,
+        ),
+        lightTheme: SettingsThemeData(
           settingsTileTextColor: Theme.of(context).textTheme.bodyText2!.color,
           titleTextColor: Theme.of(context).iconTheme.color,
           settingsListBackground: Theme.of(context).backgroundColor,
@@ -40,18 +54,54 @@ class _MainSettingsPageState extends State<MainSettingsPage> {
                 leading: const Icon(Icons.format_paint),
                 title: const Text('Display previous period'),
               ),
-              SettingsTile.switchTile(
-                onToggle: (value) {
-                  setState(() {
-                    widget.settings.autoPlayPeriodsAnimation = value;
-                  });
+              SettingsTile.navigation(
+                title: Text("Theme"),
+                onPressed: (context) {
+                  final color = Colors.primaries[
+                      math.Random().nextInt(Colors.primaries.length)];
+                  widget.root().setDarkTheme(
+                        Themes.themeFromPrimaryColor(color, ThemeMode.dark),
+                      );
+                  widget.root().setLightTheme(
+                        Themes.themeFromPrimaryColor(color, ThemeMode.light),
+                      );
                 },
-                initialValue: widget.settings.autoPlayPeriodsAnimation,
-                leading: const Icon(Icons.format_paint),
-                title: const Text('Auto play periods animation'),
               ),
+              SettingsTile.switchTile(
+                  initialValue: widget.settings.darkMode,
+                  onToggle: (value) {
+                    widget.settings.darkMode = value;
+                    setState(() {});
+                    widget.root().refresh();
+                  },
+                  title: Text("Dark mode"))
             ],
           ),
+          SettingsSection(title: const Text("Timetable"), tiles: [
+            SettingsTile.switchTile(
+              onToggle: (value) {
+                setState(() {
+                  widget.settings.timetableAutoPlayPeriodsAnimation = value;
+                });
+              },
+              initialValue: widget.settings.timetableAutoPlayPeriodsAnimation,
+              leading: const Icon(Icons.autofps_select_sharp),
+              title: const Text('Auto play periods animation'),
+            ),
+            SettingsTile.navigation(
+              leading: const Icon(Icons.timer_sharp),
+              title: const Text("Auto play interval"),
+              onPressed: (context) {
+                Navigator.of(context).pop();
+              },
+            ),
+            SettingsTile.switchTile(
+              initialValue: widget.settings.timetableEnableInfiniteScroll,
+              onToggle: (value) => setState(
+                  () => widget.settings.timetableEnableInfiniteScroll = value),
+              title: const Text("Enable infinite scroll"),
+            )
+          ]),
         ],
       ),
     );
